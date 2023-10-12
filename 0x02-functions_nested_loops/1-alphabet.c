fe029dbe8da36a978343afc3b68f81eceb2cb9d5
Salvage <salvageblaine@gmail.com>
2023-10-11 22:16:54 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - function to print alphabets in lower case
+* Return: return 0
+*/
+
+void print_alphabet(void)
+{
+
+char c;
+
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
