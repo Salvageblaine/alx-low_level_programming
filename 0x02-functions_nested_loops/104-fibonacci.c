598487c79265efe5bd3ba569f7708ee870076d09
Salvage <salvageblaine@gmail.com>
2023-10-12 11:47:25 -0700

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,9 +2,9 @@
 
 
 /**
- * main - The main function
- * 1 and 2, seperated by a comma
- * Return: 0.
+ * main - prints th firdt 98 Fibonacci numbers, starting with
+ * 1 and 2, seperated by a comma followed by a space.
+ * Return: Always 0
  */
 
 
@@ -15,7 +15,6 @@ int main(void)
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
-
 
 	for (count = 0; count < 92; count++)
 	{
