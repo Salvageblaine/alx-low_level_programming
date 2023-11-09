617a62b0937b7c05cf34d2b19e7b0f6b39440a8f
Salvage <salvageblaine@gmail.com>
2023-11-08 19:20:17 -0800

    arrays to be printed

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include < stdlib>
+
+/**
+ * array_iterator - executes function on each element of array
+ * @array: the given array
+ * @size: the size of the given array
+ * @action: the function to be performed on each element of array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
