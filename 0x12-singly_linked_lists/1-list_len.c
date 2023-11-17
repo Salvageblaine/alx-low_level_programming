282e969659b74c778a39ef4418e7af66624eb400
Salvage <salvageblaine@gmail.com>
2023-11-16 20:02:09 -0800

    listing the length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - it returns the number of elements
+ * @h: singly linked lists.
+ *
+ * Return: the number of elements in a list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while  (h != NULL)
+	{
+		h = h->next;
+		elements++;
+	}
+	return (elements)
+}
